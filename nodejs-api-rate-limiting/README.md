7ee0e203403a1c0efa2441bf1d9920128f774954
Jenis Gadhiya <jenugadhiya8@gmail.com>
2024-07-03 15:17:08 +0530

    performed api rate limit practical demo

---
+++ nodejs-api-rate-limiting/README.md
@@ -0,0 +1,22 @@
+# Node.js API Rate Limiting Demo Project
+
+## Getting Started
+
+### Prerequisites
+
+Make sure you have Node.js and npm installed on your machine.
+Then, install the project's dependencies with:
+
+```bash
+npm install
+```
+
+### Start the Application
+
+```bash
+npm run start
+```
+
+### Test the API
+
+Contact the `GET` endpoint at `http://localhost:3000/hello-world`. This should return the "Hello, World!" string, following a specific rate-limiting behavior.
---
+++ nodejs-api-rate-limiting/index.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const { rateLimit } = require("express-rate-limit");
+
+const port = 3000;
+
+// initialize an Express server
+const app = express();
+
+// define the rate limiting middleware
+const limiter = rateLimit({
+  windowMs: 5 * 60 * 1000, // 5 minutes
+  limit: 10, // each IP can make up to 10 requests per `windowsMs` (5 minutes)
+  standardHeaders: true, // add the `RateLimit-*` headers to the response
+  legacyHeaders: false, // remove the `X-RateLimit-*` headers from the response
+  message: "Too many requests", // customize the message on error of rate limit
+});
+
+// apply the rate limiting middleware to all endpoints
+app.use(limiter);
+
+// define a sample endpoint
+app.get("/hello-world", (req, res) => {
+  res.send("Hello, World!");
+});
+
+// start the server
+app.listen(port, () => {
+  console.log(`Server listening at http://localhost:${port}`);
+});
